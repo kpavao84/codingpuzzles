92d2c03f60f78c7f31e6a9971aee6af2f33595eb
Kevin Pavao <kpavao84@gmail.com>
2017-12-03 01:18:15 -0600

    whitespace and comments

--- day1.scm
+++ day1.scm
@@ -1,15 +1,19 @@
+;; Integer -> Integer
 (define (inverse-captcha captcha)
+  ;; List -> Bool
   (define (first-last-match? captcha-list)
     (= (car captcha-list) (car (reverse captcha-list))))
+
+  ;; List Integer -> Integer
   (define (iter captcha-list current)
     (cond ((null? (cdr captcha-list)) current)
           ((= (car captcha-list) (cadr captcha-list))
-           (iter (cdr captcha-list)
-                 (+ (car captcha-list) current)))
+           (iter (cdr captcha-list) (+ (car captcha-list) current)))
           (else
            (iter (cdr captcha-list) current))))
+
   (if (first-last-match? (number->list captcha))
-      (iter (number->list captcha)  (car (number->list captcha)))
+      (iter (number->list captcha) (car (number->list captcha)))
       (iter (number->list captcha) 0)))
 
 ;from https://stackoverflow.com/questions/8014453/convert-number-to-list-of-digits
@@ -20,6 +24,7 @@
           (loop (quotient n b)
                 (cons (modulo n b) d))))))
 
+;; tests
 (= (inverse-captcha 1122) 3)
 (= (inverse-captcha 1111) 4)
 (= (inverse-captcha 1234) 0)
