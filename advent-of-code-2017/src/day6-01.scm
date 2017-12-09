7223a1a98e66e8123ae2693152f1989d52e201b3
Kevin Pavao <kpavao84@gmail.com>
2017-12-09 16:46:37 -0600

    works with test data, doesnt work with actual input

--- advent-of-code-2017/src/day6-01.scm
+++ advent-of-code-2017/src/day6-01.scm
@@ -7,39 +7,51 @@
     (let ((line (read-line)))
       (map (lambda (x) (string->number x)) (string-split line #\tab))))
 
-  ;; List (List List) Integer -> Integer
+  ;; (list Integer) (list (list Integer)) Integer -> Integer
   ;; return the amount of redistribution cycles that can be done
   ;; before a repeat configuration is found
   (define (reallocate state history cycles)
     (let ((max-bank (find-nth state (apply max state))))
       (if (member state history)
           cycles
-          (reallocate (redistribute state
-                                    max-bank
-                                    (if (= max-bank (1- (length state)))
-                                        max-bank
-                                        0))
+          (reallocate (redistribute state max-bank)
                       (cons state history)
                       (1+ cycles)))))
 
-  ;; List Integer -> List
-  ;; redistrubute the memory blocks until the given bank is 1
+  ;; (list Integer) Integer -> List
+  ;; redistrubute the memory blocks until the given bank is equal to end 
   ;; then return the state
-  (define (redistribute state max-bank next-bank)
-    (if (= (list-ref state max-bank) 1)
-        state
-        (redistribute
-         (increase-next (decrease-largest state max-bank) next-bank)
-         max-bank
-         (if (< next-bank (1- (length state)))
-             (1+ next-bank)
-             0))))
+  (define (redistribute state max-bank)
+    (let ((state-length (length state)))
+      (define (redistribute state next-bank end)
+        (if (= (list-ref state max-bank) end)
+            state
+            (redistribute
+             (increase-next (decrease-largest state max-bank) next-bank)
+             (get-next next-bank)
+             end)))
+
+      ;; Integer
+      (define (get-next current-bank)
+        (if (< current-bank (1- state-length))
+            (1+ current-bank)
+            0))
+
+      (redistribute state (get-next max-bank) (if (max-tied? state) 0 1))))
+
+  ;; (list Integer) -> Bool
+  ;; check to see if the max element appears twice in a list
+  (define (max-tied? state)
+    (let ((max-value (apply max state)))
+      (> (length
+          (filter (lambda (x) (= x max-value)) state))
+         1)))
 
   (define (increase-next state next-bank)
-    (modify-nth state next-bank 1+))
+    (modify-nth 1+ state next-bank))
 
   (define (decrease-largest state max-bank)
-    (modify-nth state max-bank 1-))
+    (modify-nth 1- state max-bank))
 
   (let ((state (get-memory-banks)))
     (reallocate state '() 0)))
@@ -66,5 +78,5 @@
 
 ;; List Integer Fn -> List
 ;; perform an operation on the value at the given index of a list
-(define (modify-nth lst index op)
+(define (modify-nth op lst index)
   (set-nth lst index (op (list-ref lst index))))
